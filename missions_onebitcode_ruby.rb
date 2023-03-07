df4e584072eccfdf4bae06c73665403723fbd914
manoel bertin <manoeltirj@gmail.com>
2023-03-07 08:13:26 -0300

    my first commit comented of code

---
+++ missions_onebitcode_ruby.rb
@@ -0,0 +1,52 @@
+# Manoel Bertin
+# há 9 meses (editado)
+# Resolução das 3 Missões:
+
+# Missão 1
+array = []
+
+puts "Escolha 3 números para elevar à segunda potência:"
+print "Primeiro número => "
+array << first_number = gets.chomp.to_i
+print "Segundo número => "
+array << second_number = gets.chomp.to_i
+print "Terceiro número => "
+array << third_number = gets.chomp.to_i
+
+
+new_array = array.map do |number|
+    square = number ** 2
+end
+
+puts ("A segunda potência dos valores informados é: #{new_array}")
+
+# # #Missão 2
+
+# hash = {}
+# 3.times do
+#     puts "Informe um estado: "
+#     city = gets.chomp
+#     puts "Informe uma capital: "
+#     hash["#{city}"] = gets.chomp
+# end 
+
+# hash.each do |city, capital|
+#     puts "A capital do estado de #{city} é #{capital}"
+#   end
+
+# #Missao 3
+
+# numbers = {a: 10, b: 30, c: 20, d:25, e: 15}
+# key_bigger_number = 0
+# flag = 0
+
+# bigger_value = Hash.new
+# numbers.each do |key, value|
+#     if value > flag
+#         bigger_value = value
+#         flag = value
+#         key_bigger_number = key
+#     end
+# end
+# puts "O maior número é: #{flag}"
+# puts "A chave desse nº é: #{key_bigger_number}"
