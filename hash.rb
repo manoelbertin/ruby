1b7188fe2db65c26764de04751e33fc68d120137
manoel bertin <manoeltirj@gmail.com>
2023-03-14 07:00:03 -0300

    concatenei strings com variáveis do tipo hash

--- hash.rb
+++ hash.rb
@@ -4,12 +4,12 @@ capitaisBBB = Hash.new
 
 capitais = { acre: 'Rio Branco', sao_paulo: 'São Paulo'}
 
-puts capitaisAAA
-puts capitaisBBB
-puts capitais
+puts "capitaisAAA = #{capitaisAAA}"
+puts "capitaisBBB = #{capitaisBBB}"
+puts "capitais = #{capitais}"
 
-puts "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+puts "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
 
 capitais[:minas_gerais] = 'Belo Horizonte'
-puts capitais
+puts "capitais = #{capitais}"
 
