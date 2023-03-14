1833d5bf18977128567126d649b34526b3b6dee2
manoel bertin <manoeltirj@gmail.com>
2023-03-14 09:35:55 -0300

    utilizando o select com o hash

--- select.rb
+++ select.rb
@@ -1,5 +1,7 @@
 # select - ajuda a gente a fazer pesquisas dentro de arrays ou rashes
 
+# coisas que tem o mesmo preço ou mesmo sobrenome SILVA por exemplo
+
 array = [1, 2, 3, 4, 5, 6]
 
 selection = array.select do |a|
@@ -7,3 +9,12 @@ selection = array.select do |a|
 end
 
 puts selection
+
+hash = {0 => 'zero', 1 => 'um', 2 => 'dois', 3 => 'três'}
+select_key = hash.select do |key, value|
+  key > 0
+end
+puts '\n selecione os elementos que a chave é maior do que zero'
+puts "\n selecione os elementos que a chave é maior do que zero #{hash}"
+
+puts select_key
