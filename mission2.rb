9668e86850863089b5621be3a95111739ef0114c
manoel bertin <manoeltirj@gmail.com>
2023-03-15 07:50:10 -0300

    segunda missão feita aperfeiçoada

--- mission2.rb
+++ mission2.rb
@@ -2,3 +2,19 @@
 # informando a chave e o valor. no final do programa para cada um desses
 # elementos imprima a frase "uma das chaves é ... e o seu valor é ..."
 
+hash = {}
+c = 1
+
+while c <= 3 do 
+  print "Digite uma chave  "
+  key = gets.chomp
+  print "Digite um  valor  "
+  hash["#{key}"] = gets.chomp
+  c += 1
+end
+puts "=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-="
+
+hash.each do |key, value|
+    puts "Uma das chaves é #{key} e seu valor #{value}"
+  end
+puts "FIM"
