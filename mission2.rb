20f50947309fa15d74f93a9c7873066af9c8ae0b
manoel bertin <manoeltirj@gmail.com>
2023-03-15 07:08:31 -0300

    segunda missão utilizando hash

---
+++ mission2.rb
@@ -0,0 +1,4 @@
+# crie uma collection do tipo hash e permita que o usuario crie 3 elementos
+# informando a chave e o valor. no final do programa para cada um desses
+# elementos imprima a frase "uma das chaves é ... e o seu valor é ..."
+
