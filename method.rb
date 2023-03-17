c7c12df35b7087fd855a19bede9e53e87908662e
manoel bertin <manoeltirj@gmail.com>
2023-03-17 09:45:33 -0300

    first commit about method

---
+++ method.rb
@@ -0,0 +1,10 @@
+# métodos são uma forma de organizar instruções em um programa,
+# permitindo que trechos de código sejam reutilizados
+#
+
+def hello(name= "('type your name')")
+  puts "Hello #{name}, how are you?"
+end
+
+hello("Manoel")
+#hello
