70d4fe3f6f591eeb9a2a551ee5a1720377890a75
manoel bertin <manoeltirj@gmail.com>
2023-03-20 18:04:15 -0300

    conceitos poo

---
+++ poo.rb
@@ -0,0 +1,10 @@
+# POO é um paradigma de programação criado para lidar com softwares grandes e complexos.
+# É um conceito seguido não só pelo Ruby, mas também por várias outras linguagens
+# de programação como Java, Python, C++ etc.
+
+# Merge seria mesclar uma classe com a outra classe através de herança de classes (reaproveitamento)
+
+# Objeto tem atributos(informações) e métodos(comportamentos ou ação)-> attributes and methods.
+
+
+
