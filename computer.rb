7d97d3e6e5dd98c1fde1d09caaa423b42e3a718d
manoel bertin <manoeltirj@gmail.com>
2023-03-20 18:12:30 -0300

    class computer created

---
+++ computer.rb
@@ -0,0 +1,14 @@
+# poo exemple:
+
+class Computer
+  def turn_on
+    'turn on the computer'
+  end
+
+  def shutdown
+    'shutdown the computer'
+  end
+end
+
+computer = Computer.new
+puts computer.shutdown
