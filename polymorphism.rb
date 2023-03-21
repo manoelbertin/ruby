6a80cb7dd55f1363bb61695a188d867ee27b8aac
manoel bertin <manoeltirj@gmail.com>
2023-03-21 17:29:44 -0300

    polymorphism

---
+++ polymorphism.rb
@@ -0,0 +1,31 @@
+class Instrument
+  def write
+    puts 'Writing.'
+  end
+end
+
+class Keyboard < Instrument
+end
+
+class Pencil < Instrument
+  def write
+    puts 'Write with a pencil.'
+  end
+end
+
+class Pen < Instrument
+  def write
+    puts 'Write with a pen.'
+  end
+end
+
+keyboard = Keyboard.new
+pencil = Pencil.new
+pen = Pen.new
+
+puts "pencil:"
+pencil.write
+puts "pen:"
+pen.write
+puts "keyboard  overwrite below:"
+keyboard.write
