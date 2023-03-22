84eedbbc7ca40298d181831429be18a8b16f6555
manoel bertin <manoeltirj@gmail.com>
2023-03-22 13:02:27 -0300

    error because not put @

---
+++ instance.rb
@@ -0,0 +1,16 @@
+# instance variable
+
+class User
+  def add(name)
+    @name = name
+    puts "User added!!!"
+    hello
+end
+
+  def hello
+    puts "Welcome '#{name}'."
+  end
+end
+
+user = User.new
+user.add('João')
