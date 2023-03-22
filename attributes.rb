e1d9515fa5e882927bf84575c3db2acd773eb6b5
manoel bertin <manoeltirj@gmail.com>
2023-03-22 13:13:36 -0300

    notar que name= são juntos

--- attributes.rb
+++ attributes.rb
@@ -1,2 +1,18 @@
 # Atributos =>> (ou variáveis de instância) no Ruby são sempre PRIVADOS E
 # começam com @, eles só podem ser alterados pelos métodos de um objeto.
+
+class Dog
+
+  def name
+    @name
+  end
+
+  def name= name
+    @name = name
+  end
+
+end
+
+dog = Dog.new
+dog.name = 'Merlim'
+puts dog.name
