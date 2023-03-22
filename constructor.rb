fcdf8dd15dec0996245af2dfe0bd00d362d230a7
manoel bertin <manoeltirj@gmail.com>
2023-03-22 15:14:02 -0300

    with paramethers

--- constructor.rb
+++ constructor.rb
@@ -1,13 +1,13 @@
 class Person
 
-  def initialize(name, age)
+  def initialize(name='Enter name', age='Enter age')
     @name = name
     @age = age
   end
 
   def check
     puts "Class Instance started with values:"
-    puts "Name: #{@name}"
+    puts "Name: #{@name}" # here and ...
     puts "Age: #{@age} years old" # aqui passo as 2 variáveis de instância com o @
   end
 
@@ -15,3 +15,6 @@ end
 
   person2 = Person.new('Ary', 21) # atenção aqui que passo parametros
   person2.check
+puts ''
+  person1 = Person.new
+  person1.check
