52b7723ca4e1642dfba6a85d2e4a85bc51e8d802
manoel bertin <manoeltirj@gmail.com>
2023-03-22 13:20:21 -0300

    aqui estou acessando os atributos

--- attributes.rb
+++ attributes.rb
@@ -3,16 +3,16 @@
 
 class Dog
 
-  def name
+  def name # leio uma variável de instancia, veja são 2 métodos diferentes. name e name=
     @name
   end
 
-  def name= name
-    @name = name
+  def name= name # vai escrever essa variável de instancia chamada name
+    @name = name # aqui eu seto esse valor
   end
 
 end
 
-dog = Dog.new
-dog.name = 'Merlim'
+dog = Dog.new # instanciando objeto
+dog.name = 'Merlim' # preenchendo o atributo do objeto instanciado
 puts dog.name
