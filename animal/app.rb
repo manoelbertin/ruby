dc3eacaa28cf36d4d6a4390a8974232f72a23df0
manoel bertin <manoeltirj@gmail.com>
2023-03-22 09:22:00 -0300

    differences of requires

---
+++ animal/app.rb
@@ -0,0 +1,5 @@
+require './animal.rb'
+
+animal = Animal.new # created the instance
+animal.jump         # imported the method
+animal.sleep
---
+++ animal/requere.rb
@@ -0,0 +1,3 @@
+# require './animal.rb' ==> tenho que estar dentro da pasta animal para executar o app.rb
+
+# require_relative 'animal' => NÃO leva mais em conta da onde eu estou executando.
