b16026a13fa270edb451db4cc88e4dbd4c16b110
manoel bertin <manoeltirj@gmail.com>
2023-03-22 12:19:37 -0300

    types of variables

---
+++ animal/escopo_variaveis_4tipos.rb
@@ -0,0 +1,17 @@
+# ESCOPO DE VARIÁVEIS
+
+# 1- Variável Local =>>
+# É declarada com a primeira letra de seu nome sendo uma letra minúscula.
+#   Ela pode ser acessada apenas onde foi criada.
+# Por exemplo:
+# se voce definir uma variável local dentro de uma classe, ela estará disponível apenas
+# dentro desta classe, se a definiu dentro de um método, conseguirá acessá-la apenas dentro
+# deste método e assim por diante.
+#
+# 2- Variável Global =>>
+# Declarada com o prefixo $
+#
+# Pode ser acessada em qualquer lugar do programa. Seu uso é FORTEMENTE DESENCORAJADO pois além 
+# de ser visível em qualquer lugar do código, também pode ser alterada em inúmeros locais
+# ocasionando dificuldades no rastreamento de bugs.
+
