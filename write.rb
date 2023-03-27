dda8b6b15d0d0699ac965086401a004016db4b17
manoel bertin <manoeltirj@gmail.com>
2023-03-27 15:57:42 -0300

    printed exemple

---
+++ write.rb
@@ -0,0 +1,16 @@
+File.open('shopping-list.txt', 'a') do |line|
+  line.puts('rice')
+  line.puts('beans')
+  line.print('portuguese')
+  line.print(' olive ')
+  line.print('oil')
+end
+
+# --Shopping List--
+# strawberry
+# broccoli
+# palm heart
+# tomatorice
+# beans
+# portuguese olive oil
+# --End of program--
