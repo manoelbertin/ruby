a85902918ae00913d3cb861ec3a0f8bd25c960be
manoel bertin <manoeltirj@gmail.com>
2023-03-27 18:11:39 -0300

    consult the website

--- web_get.rb
+++ web_get.rb
@@ -2,6 +2,7 @@ require 'uri'
 require 'net/http'
 
 # example.html clico no gerenciador de arquivos que mostra a página web !!!
+# https://www.twilio.com/blog/5-ways-make-http-requests-ruby  <<<--- consultar aqui
 uri = URI('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
 res = Net::HTTP.get_response(uri)
 File.open('exemple.html', 'w') do |line|
